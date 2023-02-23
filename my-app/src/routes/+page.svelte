bb1cf0db6103b36ca0d430293003a918117e73ac
Reuben McArley (1000113227) <MCARRL1@student.op.ac.nz>
2023-02-24 11:30:11 +1300

    save

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,2 +1,52 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!--CSS style-->
+<style>
+
+    button{
+        background-color:black;
+        color: yellow;
+        border:none;
+        padding:3px;
+    }
+    h1{
+        color:blue;
+        font-size:34px;
+        font-style:italic;
+    }
+</style>
+
+<!--JAVASCRIPT functionality-->
+<script>
+    let count = 0;
+    let count2 = 1;
+    let audio;
+    function incrementCount()
+    {
+        count++;
+    }
+
+    function buttonSize()
+    {
+        let pad = document.getElementById("increaseSize");
+        count2++;
+        pad.style.padding = (count2)+"vw";
+        pad.style.fontSize = (count2)+"rem";
+    }
+
+
+    function buttonSmall()
+    {
+        let pad = document.getElementById("increaseSize");
+        if (count2 != 1){
+            count2 = count2-1;
+        }
+        
+        pad.style.padding = (count2)+"vw";
+        pad.style.fontSize = (count2)+"rem";
+    }
+</script>
+
+<!--HTML structure-->
+<button on:click={incrementCount} id = "increaseSize" on:click={buttonSize} on:keydown={buttonSmall}>
+    Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+
