7f2df42eb949c125813cc21d88effeeff41a09cf
Reuben McArley (1000113227) <MCARRL1@student.op.ac.nz>
2023-03-31 11:39:49 +1300

    add images, fix positioning

--- assignment/src/lib/header.svelte
+++ assignment/src/lib/header.svelte
@@ -3,6 +3,7 @@
 </script>
 
 <header class="overlay">
-  <img src={HeaderImage} alt="headerImage" style="width:100%;"/>
-  <h1 class="centeredText">COFFEE</h1>
+  <img src={HeaderImage} alt="headerImage" style="width: auto 0;"/>
+  <h1 class="centeredTitle">COFFEE</h1>
+  <h2 class="centeredDesc">A website dedicated to early mornings</h2>
 </header>
--- assignment/src/routes/+page.svelte
+++ assignment/src/routes/+page.svelte
@@ -2,6 +2,30 @@
   import Card from "$lib/card.svelte";
   const cardTitle = "Example Card";
   const cardContent = "This is some example content for the card.";
+
+  import Coffee1 from "$lib/assets/christina-rumpf-LMzwJDu6hTE-unsplash.jpg";
+  import Coffee2 from "$lib/assets/clay-banks-_wkd7XBRfU4-unsplash (1).jpg";
+  import Coffee3 from "$lib/assets/ante-samarzija-lsmu0rUhUOk-unsplash.jpg";
+  import Coffee4 from "$lib/assets/devin-avery-5iRgh_G0eRY-unsplash.jpg";
+  import Coffee5 from "$lib/assets/nathan-dumlao-c2Y16tC3yO8-unsplash.jpg";
+  import Coffee6 from "$lib/assets/nathan-dumlao-nBJHO6wmRWw-unsplash.jpg";
+  import Coffee7 from "$lib/assets/nathan-dumlao-pMW4jzELQCw-unsplash.jpg";
+  import Coffee8 from "$lib/assets/nathan-dumlao-Y3AqmbmtLQI-unsplash.jpg";
 </script>
 
 <Card title="Hello" content="Goodbye" />
+<br />
+  <div class="row">
+    <div class="column">
+      <img src={Coffee1} alt="coffee1" />
+      <img src={Coffee2} alt="coffee2" />
+      <img src={Coffee3} alt="coffee3" />
+      <img src={Coffee4} alt="coffee4" />
+    </div>
+    <div class="column">
+      <img src={Coffee5} alt="coffee5" />
+      <img src={Coffee6} alt="coffee6" />
+      <img src={Coffee7} alt="coffee7" />
+      <img src={Coffee8} alt="coffee8" />
+    </div>
+  </div>
