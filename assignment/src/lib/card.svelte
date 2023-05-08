1f13d55a7dac27a8850bfdda8c2152a350e55932
Reuben McArley (1000113227) <MCARRL1@student.op.ac.nz>
2023-05-09 09:57:44 +1200

    card and header fixes

--- assignment/src/lib/card.svelte
+++ assignment/src/lib/card.svelte
@@ -1,26 +1,30 @@
 <script>
   export let title;
   export let content;
+  export let image;
 </script>
 
 <div class="card">
+  <img class="image"src={image} alt={title}/>
   <h3 class="card-title">{title}</h3>
   <div class="card-content">{content}</div>
+  
 </div>
 
 <style>
   .card {
     background-color: #ffffff;
     border: 1px solid #e6e6e6;
-    border-radius: 4px;
+    border-radius: 8px;
     box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05), 0 2px 2px rgba(0, 0, 0, 0.05),
       0 4px 4px rgba(0, 0, 0, 0.05), 0 8px 8px rgba(0, 0, 0, 0.05),
       0 16px 16px rgba(0, 0, 0, 0.05);
     padding: 0.75rem;
-    width: 250px;
-    float: left;
-    margin: 0 0 1rem 1rem;
-    margin-left: 35rem;
+    width: 250px; /*make this not pixels*/
+    /*float: left;*/
+    margin: 0 3rem 0rem 3rem;
+    font-family: "Oswald", sans-serif;
+    justify-content: center;
   }
   .card-title {
     margin-top: 0;
@@ -29,5 +33,11 @@
   .card-content {
     margin-bottom: 0;
     font-size: 1rem;
+  }
+
+  .image{
+    padding: 0px;
+    margin: 0px;
+    width: 250px; /*make this not pixels*/
   }
 </style>
--- assignment/src/routes/+page.svelte
+++ assignment/src/routes/+page.svelte
@@ -1,21 +1,37 @@
 <script>
   import Card from "$lib/card.svelte";
   import Map from "$lib/map.svelte";
+  import Image from "$lib/assets/oddSize.jpg";
   const cardTitle = "Example Card";
   const cardContent = "This is some example content for the card.";
 </script>
 
-<Card
-  title="Coffee"
-  content="
+<div class="cardLayout">
+  <Card
+    title=""
+    content="
   Welcome to our coffee website, where we celebrate the art and science of brewing and enjoying coffee. 
   For many of us, coffee is more than just a morning pick-me-up - it's a daily ritual that brings joy, 
-  comfort, and inspiration. Whether you're a seasoned coffee connoisseur or just starting to explore 
-  the world of specialty coffee, our website offers a wealth of resources, tips, and inspiration to 
-  help you take your coffee game to the next level. From expert brewing techniques and flavor profiles
-   to coffee shop reviews and industry news, we're here to share our love of coffee and help you discover
-    new ways to savor every cup. So grab your favorite mug, settle in, and let's dive into the wonderful world of coffee together!"
-/>
-<br>
+  comfort, and inspiration."
+    image={Image}
+  />
+  <Card
+    title=""
+    content="
+  Welcome to our coffee website, where we celebrate the art and science of brewing and enjoying coffee. 
+  For many of us, coffee is more than just a morning pick-me-up - it's a daily ritual that brings joy, 
+  comfort, and inspiration."
+    image={Image}
+  />
+  <Card
+    title=""
+    content="
+  Welcome to our coffee website, where we celebrate the art and science of brewing and enjoying coffee. 
+  For many of us, coffee is more than just a morning pick-me-up - it's a daily ritual that brings joy, 
+  comfort, and inspiration."
+    image={Image}
+  />
+</div>
+<br />
 <Map />
-<br>
+<br />
